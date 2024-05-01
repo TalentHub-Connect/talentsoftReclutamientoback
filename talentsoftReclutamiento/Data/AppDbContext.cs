83011f94acbc5ae4d14737be166fcc313041c4bf
Felipe <juanfeariascastillo@live.com>
2024-05-01 04:38:10 -0500

    Servicios básicos.

---
+++ talentsoftReclutamiento/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using talentsoftReclutamiento.Models;
+//using talentsoftReclutamiento.Models;
+
+namespace talentsoftReclutamiento.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Candidate> candidate {  get; set; }
+
+        public DbSet<Offer> offer { get; set; }
+
+    }
+}
---
+++ talentsoftReclutamiento/Models/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace talentsoftReclutamiento.Models;
+
+public partial class Candidate
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public int PhoneNumber { get; set; }
+
+    public string EmploymentExchange { get; set; } = null!;
+
+    public bool Status { get; set; }
+
+    public int Curriculum_id { get; set; }
+
+    public int TalentSoftUser_id { get; set; }
+
+}
